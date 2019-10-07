d9f14bfe1acf57d5fc6f8dcc2c01751e18a68c21
hungltph03955 <hungltph03955@fpt.edu.vn>
2019-10-07 21:07:26 +0700

    code base

---
+++ app/Http/Controllers/User/UserController.php
@@ -0,0 +1,137 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\ApiController;
+use Illuminate\Support\Facades\Validator;
+use App\User;
+
+class UserController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::all();
+        return $this->showAll($users);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+      $rule = [
+        'name' => 'required',
+        'email' => 'required|email|unique:users',
+        'password' => 'required|min:6|confirmed',
+      ];
+
+      $this->validate($request, $rules);
+
+      $data = $request->all();
+      $data['password'] = bcrypt($request->password);
+      $data['verified'] = User::UNVERIFIED_USER;
+      $data['verification_token'] = User::generateVerificationCode();
+      $data['admin'] = User::REGULAR_USER;
+      $user = User::create($data);
+      return response()->json(['data' => $user], 200);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+      $user = User::findOrFail($id);
+      return $this->showOne($user);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+      $user = User::findOrFail($id);
+      $rules = [
+        'email' => 'email|unique:users,email,' .$user->id,
+        'password' => 'min:6|confirmed',
+        'admin' => 'in:' . User::ADMIN_USER . ',' . User::REGULAR_USER,
+      ];
+      $this->validate($request, $rules);
+
+      if ($request->has('name')) {
+        $user->name = $request->name;
+      }
+      if ($request->has('email') && $user->email != $request->email) {
+        $user->verified = User::UNVERIFIED_USER;
+        $user->verification_token = User::generateVerificationCode();
+        $user->email = $requuest->email;
+      }
+
+      if ($request->has('password')) {
+        $user->password = bcrypt($request->password);
+      }
+
+      if ($request->has('addmin')) {
+        if (!$user->isVerified()) {
+          return $this->errorResponse('Only verified users can modify the admin field', 409);
+        }
+        $user->admin = $request->admin;
+      }
+
+      if (!$user->isDirty()) {
+        return $this->errorResponse('You need to specify a different value to update', 422);
+      }
+      $user->save();
+      return $this->showOne($user);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+      $user = User::findOrFail($id);
+      $user->delete();
+      return $this->showOne($user);
+    }
+}
---
+++ routes/api.php
@@ -0,0 +1,25 @@
+<?php
+
+use Illuminate\Http\Request;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| is assigned the "api" middleware group. Enjoy building your API!
+|
+*/
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::resource('buyers','Buyer\BuyerController');
+Route::resource('categories','Category\CategoryController',['only' => ['index', 'show']]);
+Route::resource('products','Product\ProductController',['only' => ['index', 'show']]);
+Route::resource('transactions','Transaction\transactionController',['only' => ['index', 'show']]);
+Route::resource('user','User\UserController');
+Route::resource('sallers','Seller\SellerController',['only' => ['index', 'show']]);
